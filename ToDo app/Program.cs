54f0d0efe66cf29aa52b3d53aa320e03dc406951
navyanidamanuri <89773947+navyanidamanuri@users.noreply.github.com>
2021-09-22 21:56:15 +0200

    Add project files.

---
+++ ToDo app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToDo_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
